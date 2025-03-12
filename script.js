392ab5618b994a1f48c13732802b021f8143df65
Nidhi Gothwal <165782089+nidhigothwal@users.noreply.github.com>
2025-03-12 17:30:54 +0530

    update code

--- script.js
+++ script.js
@@ -5,28 +5,40 @@ console.log('js file is working')
 document.querySelector('#fromsubmit').addEventListener('click', function () {
     if (document.querySelector('#firstname').value === '') {
         document.querySelector('.error').innerText = 'First Name is Wrong.'
-    } else {
+        return;
+    }
+    else {
         document.querySelector('.error').innerText = ''
     }
 
 
     if (document.querySelector('#lastname').value === '') {
         document.querySelector('.error').innerText = 'Last Name is Wrong.'
-    } else {
+        return;
+    }
+    else {
         document.querySelector('.error').innerText = ''
     }
 
     if (document.querySelector('#phonenumber').value === '') {
         document.querySelector('.error').innerText = 'Phone Number is Wrong.'
+        return;
     } else {
         document.querySelector('.error').innerText = ''
     }
 
     if (document.querySelector('#address').value === '') {
         document.querySelector('.error').innerText = 'Address is Wrong.'
+        return;
     } else {
         document.querySelector('.error').innerText = ''
     }
 
+    if (document.querySelector('#color').value === '') {
+        document.querySelector('.error').innerText = ''
+        return;
+    } else {
+        document.querySelector('#color').innerText = 'Successfully Submitted'
+    }
 
 })
